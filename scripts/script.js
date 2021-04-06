dd01ef17b59fe9473240127b0b1c0b5d7f683192
sophiejcyoung <78481542+sophiejcyoung@users.noreply.github.com>
2021-04-06 22:40:06 +0200

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,21 @@
 // JavaScript Document
+//jslint browser: true, devel: true, eqeq: true, plusplus: true, sloppy: true, vars: true, white: true/
+//eslint-env browser/
+//eslint 'no-console':0/
+
+/* eslint-env es6 */
+/* eslint-disable */
+
+var menu = document.querySelector("button");
+var menuInhoud = document.querySelector("#menu");
+var sluitKnop = document.querySelector("#sluiten");
+
+menu.addEventListener("click", function () {
+	menuInhoud.classList.add("show");
+});
+
+sluitKnop = document.querySelector("click", function () {
+	menuInhoud.classList.remove("show");
+});
+
+//bronnen:https://www.youtube.com/watch?v=cUXcVWHhsJs, https://www.w3schools.com/howto/howto_js_mobile_navbar.asp, https://codepen.io/shooft/pen/wvGyvRQ, https://javascript.markkamsma.nl/hoofdstuk_7.php
